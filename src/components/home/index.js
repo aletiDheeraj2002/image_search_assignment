3990c23841e1fe163a51ba6ce5fa2843534c8bdc
dheeraj <aletidheerajkumarreddyimp@gmail.com>
2024-05-17 14:10:09 +0530

    Add pagenation

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -9,7 +9,9 @@ class Home extends Component {
     images: [],
     error: null,
     hoveredImage: null,
-    searchClicked:false
+    searchClicked:false,
+    currentPage: 1,
+    totalPages: null
   };
 
   handleInputChange = (event) => {
@@ -17,14 +19,14 @@ class Home extends Component {
   };
 
   handleSearch = async () => {
-    const { query } = this.state;
+    const { query, currentPage } = this.state;
     if (query.trim() === "") return;
 
     this.setState({ loading: true });
 
     try {
       const response = await fetch(
-        `https://api.unsplash.com/search/photos?per_page=30&page=1&query=${query}&client_id=SAiNdZ2kUfsrcHCgNk1arLfobJfsTBhl-3t8IjZstYY`
+        `https://api.unsplash.com/search/photos?per_page=30&page=${currentPage}&query=${query}&client_id=SAiNdZ2kUfsrcHCgNk1arLfobJfsTBhl-3t8IjZstYY`
       );
 
       if (!response.ok) {
@@ -32,7 +34,11 @@ class Home extends Component {
       }
 
       const data = await response.json();
-      this.setState({ images: data.results, loading: false,searchClicked:true });
+      this.setState({
+        images: data.results,
+        loading: false,
+        totalPages: data.total_pages
+      });
     } catch (error) {
       this.setState({ error: error.message, loading: false });
     }
@@ -43,16 +49,33 @@ class Home extends Component {
       this.handleSearch();
     });
   };
-  
+
   handleImageHover = (index) => {
     this.setState({ hoveredImage: index });
   };
 
+  handlePrevPage = () => {
+    this.setState((prevState) => ({
+      currentPage: prevState.currentPage - 1
+    }), () => {
+      this.handleSearch();
+    });
+  };
+
+  handleNextPage = () => {
+    this.setState((prevState) => ({
+      currentPage: prevState.currentPage + 1
+    }), () => {
+      this.handleSearch();
+    });
+  };
+
   render() {
-    const { query, loading, images, error, hoveredImage,searchClicked } = this.state;
+    const { query, loading, images, error, hoveredImage,searchClicked,currentPage, totalPages } = this.state;
     const keywords = ["Mountain", "Flowers", "Beaches", "Cities"];
 
     return (
+
       <div className="home-cont">
         <h1 className="heading">Image Search App</h1>
         <div className="searchinput-button-cont">
@@ -98,6 +121,13 @@ class Home extends Component {
             </div>
           ))}
         </div>
+        {totalPages && (
+          <div className="pagination-container">
+            <button onClick={this.handlePrevPage} disabled={currentPage === 1}>Prev</button>
+            <span>Page {currentPage} of {totalPages}</span>
+            <button onClick={this.handleNextPage} disabled={currentPage === totalPages}>Next</button>
+          </div>
+        )}
       </div>
     );
   }
